447f5a86864999e3adc082c1b62dc47f77610545
Pooya Parsa <pooya@pi0.ir>
2017-09-01 14:04:06 +0430

    fix eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const { resolve } = require('path')
+// const { resolve } = require('path')
 
 export default async function module (moduleOptions) {
-  const options = Object.assign({}, moduleOptions)
+  // const options = Object.assign({}, moduleOptions)
 
   // this.addPlugin({
   //   src: resolve(__dirname, '../templates/plugin.js'),
