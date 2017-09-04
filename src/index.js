fbd71f7072dfff0dacb69f5b7b1c44ec353d0fc0
Matteo Gabriele <matteo@blocklevel.nl>
2017-09-04 21:30:35 +0200

    refactor(options): add ua property compatibility
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const { resolve } = require('path')
 
-export default async function module (options) {
+export default async function module (moduleOptions) {
+  const options = this.options['google-analytics'] || moduleOptions
+
+  // see https://github.com/nuxt-community/analytics-module/issues/2
+  if (options.ua) {
+    options.id = options.ua
+    delete options.ua
+  }
+
   this.addPlugin({
     src: resolve(__dirname, '../templates/plugin.js'),
     options,
