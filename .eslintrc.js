85fb89d14e676be575ea16a0e526eb3d9d81e8b3
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2019-02-08 15:35:42 -0200

    style(eslintrc): use single quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: "babel-eslint",
-    sourceType: "module"
+    parser: 'babel-eslint',
+    sourceType: 'module'
   },
   extends: [
-    "@nuxtjs"
+    '@nuxtjs'
   ]
 }
