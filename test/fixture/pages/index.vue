2bd62034e46b9bf08b191aeff71a31be3b2d2d06
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-02-17 07:58:46 -0300

    test(module): use `get-port`

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-<div>
+  <div>
     Works!
-</div>
+  </div>
 </template>
 
 <script>
--- test/module.test.js
+++ test/module.test.js
@@ -1,15 +1,15 @@
 jest.setTimeout(60000)
-process.env.PORT = process.env.PORT || 5060
 
 const { Nuxt, Builder } = require('nuxt-edge')
 const request = require('request-promise-native')
+const getPort = require('get-port')
 
-const url = path => `http://localhost:${process.env.PORT}${path}`
+let nuxt, port
+
+const url = path => `http://localhost:${port}${path}`
 const get = path => request(url(path))
 
 describe('module', () => {
-  let nuxt
-
   afterEach(async () => {
     await nuxt.close()
   })
@@ -17,7 +17,8 @@ describe('module', () => {
   test('default', async () => {
     nuxt = new Nuxt(require('./fixture/nuxt.config'))
     await new Builder(nuxt).build()
-    await nuxt.listen(process.env.PORT)
+    port = await getPort()
+    await nuxt.listen(port)
     const html = await get('/')
     expect(html).toContain('Works!')
   })
@@ -25,7 +26,8 @@ describe('module', () => {
   test('with ua', async () => {
     nuxt = new Nuxt(require('./fixture/with_ua_nuxt.config'))
     await new Builder(nuxt).build()
-    await nuxt.listen(process.env.PORT)
+    port = await getPort()
+    await nuxt.listen(port)
     const html = await get('/')
     expect(html).toContain('Works!')
   })
