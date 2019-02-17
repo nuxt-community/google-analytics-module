a38083f196ac7922b61dce7c18410f2af534a05c
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-02-17 07:58:46 -0300

    test(module): use `get-port`

--- test/fixture/with_ua_nuxt.config.js
+++ test/fixture/with_ua_nuxt.config.js
@@ -1,4 +1,8 @@
+const { resolve } = require('path')
+
 module.exports = {
+  rootDir: resolve(__dirname, '../..'),
+  buildDir: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
   render: {
     resourceHints: false
